8523d9e9f8fc99dc6b3c67db01a8215511073a9e
Bimsara Hirushan <48193447+bimsaram997@users.noreply.github.com>
2024-06-30 11:24:12 +0300

    queue implemenation

---
+++ CSharpVersion/CustomQueue.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpVersion
+{
+    public class CustomQueue
+    {
+        public static readonly int MAX = 6;
+        private int frontIndex;
+        private int backIndex;
+        int[] queue = new int[MAX];
+        public CustomQueue() {
+            frontIndex = 0;
+            backIndex = 0;
+        }
+
+       public void enqueue(int data)
+        {
+            if (backIndex > MAX)
+            {
+                Console.WriteLine(" Queue over flow");
+            }
+            else {
+                queue[backIndex] = data;
+                backIndex++;
+                Console.WriteLine(data + " Inserted");
+            }
+        }
+        public void dequeue()
+        {
+            if (frontIndex == backIndex)
+            {
+                Console.WriteLine("Queue underflow");
+            }
+            else
+            {
+                int data = queue[frontIndex];
+                frontIndex++;
+                Console.WriteLine(data + " Removed");
+
+  
+                if (frontIndex == backIndex)
+                {
+                    frontIndex = 0;
+                    backIndex = 0;
+                }
+            }
+            
+        }
+
+        public int peek()
+        {
+            if(frontIndex ==  backIndex)
+            {
+                throw new InvalidOperationException("Queue is underflow");
+            } else
+            {
+                return queue[frontIndex];
+            }
+        }
+
+        public void printQueue()
+        {
+            if (frontIndex == backIndex)
+            {
+                Console.WriteLine("Queue is underflow");
+            } else
+            {
+                for(int i=frontIndex; i< backIndex; i++)
+                {
+                    Console.Write(queue[i] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+        public bool isEmpty()
+        {
+            return frontIndex == backIndex;
+        }
+    }
+}
--- CSharpVersion/Program.cs
+++ CSharpVersion/Program.cs
@@ -24,18 +24,59 @@ class Program
               Console.WriteLine(item + ",");
           }*/
 
-        CustomStack customStack= new CustomStack();
-        customStack.Push(10);
-        customStack.Push(20);
-        customStack.Push(30);
-        customStack.Push(40);
-        customStack.Push(50);
+        /*
+                CustomStack Stack
+                CustomStack customStack= new CustomStack();
+                customStack.Push(10);
+                customStack.Push(20);
+                customStack.Push(30);
+                customStack.Push(40);
+                customStack.Push(50);
 
-        customStack.PrintStack();
-        customStack.Peek();
-        Console.WriteLine("Item popped from Stack : {0}", customStack.Pop());
-        customStack.PrintStack();
+                customStack.PrintStack();
+                customStack.Peek();
+                Console.WriteLine("Item popped from Stack : {0}", customStack.Pop());
+                customStack.PrintStack();*/
 
+        /* Queue<int > queue = new Queue<int>();
+         queue.Enqueue(1);
+         queue.Enqueue(2);
+         queue.Enqueue(3);
+         queue.Enqueue(4);
+         queue.Enqueue(5);
+         queue.Dequeue();
+         Console.WriteLine(queue.Contains(5));
+
+         foreach(var item in queue)
+         {
+             Console.WriteLine(item + ", ");
+         }*/
+
+        CustomQueue queue = new CustomQueue();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+
+        queue.printQueue();
+
+        queue.dequeue();
+        queue.printQueue();
+
+        queue.enqueue(60);
+        queue.printQueue();
+
+        Console.WriteLine("Front item is: " + queue.peek());
+
+        while (!queue.isEmpty())
+        {
+            queue.dequeue();
+            queue.printQueue();
+        }
+
+        queue.dequeue();
 
     }
 }
