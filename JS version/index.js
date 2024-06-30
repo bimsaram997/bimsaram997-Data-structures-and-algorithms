7658d07bcd2f11b56bb8110c8a3c01b61cb3fff1
Bimsara Hirushan <48193447+bimsaram997@users.noreply.github.com>
2024-06-30 11:23:44 +0300

    queue implemenation

--- JS version/index.js
+++ JS version/index.js
@@ -11,3 +11,22 @@ stack.pop();
 console.log("Peek: " + stack.peek());
 console.log("Stack: " + stack.display());
 console.log("Is Empty: " + stack.isEmpty());
+
+const queue = new Queue();
+console.log(queue.enequeue(10));
+console.log(queue.enequeue(20));
+console.log(queue.enequeue(30));
+console.log(queue.enequeue(40));
+console.log(queue.enequeue(50));
+console.log(queue.enequeue(60));
+console.log(queue.enequeue(70));
+console.log(queue.enequeue(80));
+console.log(queue.peek());
+console.log(queue.dequeue());
+console.log(queue.printQueue());
+
+
+
+
+
+
